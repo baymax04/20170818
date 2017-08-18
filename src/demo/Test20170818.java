a42e79cad6f5421a9bda959a2bb7b37f08a89d6e
Administrator <Administrator@lily>
2017-08-18 18:05:32 +0800

    test20170818

---
+++ src/demo/Test20170818.java
@@ -0,0 +1,14 @@
+package demo;
+
+import java.util.Date;
+
+public class Test20170818 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(new Date());
+		System.out.println("¹þ¹þ£¬³É¹¦À²");
+	}
+
+}
